3a995765d2cca65c087f55803151a5310232f061
DavideDeLeonardis <davidedeleonardis1711@gmail.com>
2023-05-08 19:34:13 +0200

    finish logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,75 +1,68 @@
-import { ChangeEvent, ReactElement, ReactNode, useState } from 'react';
+import { FC, ChangeEvent, ReactNode, useState } from 'react';
 import './assets/scss/index.scss';
 
+type FieldValue = string | number | boolean | FieldArray | FieldObject | null;
+type FieldArray = FieldValue[];
 interface FieldObject {
-   [fieldName: string]: Field | null;
+   [fieldName: string]: FieldValue | null;
 }
+const displayFields = (field: FieldArray | FieldObject): ReactNode => {
+   return Object.entries(field).map(([fieldName, fieldValue], key) => (
+      <li key={key}>
+         <strong>{fieldName}</strong>: {typeof fieldValue} {'->'}{' '}
+         {displayFieldValue(fieldValue)}
+      </li>
+   ));
+};
 
-type Field = string | number | boolean | FieldArray | FieldObject | null;
-
-type FieldArray = Array<Field>;
-
-const App = (): ReactElement => {
-   const [json, setJson] = useState<[]>([]);
-
-   const displayFieldValue = (fieldValue: Field): string | ReactNode => {
-      if (Array.isArray(fieldValue))
-         if (
-            fieldValue.every(
-               (item) => typeof item === 'number' || typeof item === 'string'
-            )
-         )
-            return fieldValue.join(', ');
-         else
-            return fieldValue.map((item, index) => (
-               <div key={index}>
-                  {displaySingleField(item)}
-                  <br />
-               </div>
-            ));
-      else if (fieldValue && typeof fieldValue === 'object')
-         return displayFields(fieldValue);
-      else
-         return fieldValue === null || fieldValue === undefined
-            ? 'null'
-            : String(fieldValue);
-   };
-
-   const displaySingleField = (field: Field): ReactNode => {
+const displayFieldValue = (fieldValue: FieldValue): ReactNode => {
+   if (Array.isArray(fieldValue))
       if (
-         Array.isArray(field) &&
-         field.every((item) => typeof item === 'number')
+         fieldValue.every(
+            (item) => typeof item === 'number' || typeof item === 'string'
+         )
       )
-         return field.join(', ');
+         return fieldValue.join(', ');
+      else
+         return fieldValue.map((item, index) => (
+            <ul key={index}>{displaySingleField(item)}</ul>
+         ));
+   else if (fieldValue && typeof fieldValue === 'object')
+      return displayFields(fieldValue);
+   else
+      return (
+         <>
+            {fieldValue === null || fieldValue === undefined
+               ? 'null'
+               : String(fieldValue)}
+         </>
+      );
+};
 
-      if (field && typeof field === 'object')
-         return Object.entries(field as object).map(
-            ([fieldName, fieldValue], key) => (
-               <div key={key} style={{ marginLeft: '20px' }}>
-                  <strong>{fieldName}: </strong>
-                  {displayFieldValue(fieldValue as Field)}
-               </div>
-            )
-         );
+const displaySingleField = (field: FieldValue): ReactNode => {
+   if (Array.isArray(field) && field.every((item) => typeof item === 'number'))
+      return field.join(', ');
+   else if (field && typeof field === 'object') return displayFields(field);
+   else return <>{field === null || field === undefined ? 'null' : field}</>;
+};
 
-      return <>{field === null || field === undefined ? 'null' : field}</>;
-   };
-
-   const displayFields = (fields: FieldObject): ReactNode => {
-      return Object.entries(fields).map(([fieldName, fieldValue], key) => (
-         <li key={key}>
-            <strong>{fieldName}: </strong> {displayFieldValue(fieldValue)}
-         </li>
-      ));
-   };
+const App: FC = () => {
+   const [json, setJson] = useState<FieldObject[]>([]);
 
    const handleFileUpload = (e: ChangeEvent<HTMLInputElement>): void => {
+      const file = e.target.files?.[0];
+      if (!file) return;
+
       const reader = new FileReader();
       reader.onload = () => {
-         const json = JSON.parse(reader.result as string);
-         setJson(json);
+         try {
+            const json = JSON.parse(reader.result as string);
+            if (Array.isArray(json)) setJson(json);
+         } catch (error) {
+            alert('Errore nella lettura del file JSON.');
+         }
       };
-      reader.readAsText(e.target.files![0]);
+      reader.readAsText(file);
    };
 
    return (
