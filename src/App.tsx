9c62b6b276bf7b07868a4a8197493416cee163ce
DavideDeLeonardis <davidedeleonardis1711@gmail.com>
2023-05-09 13:32:46 +0200

    add textarea insertion

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 //Libraries
-import { FC, ChangeEvent, ReactNode, useState } from 'react';
+import { FC, ChangeEvent, ReactNode, useState, useRef } from 'react';
 
 // SCSS
 import './assets/scss/index.scss';
@@ -38,7 +38,6 @@ const renderFields = (field: FieldArray | FieldObject): ReactNode => {
  * @returns ReactNode
  */
 const renderFieldValue = (fieldValue: FieldValue): ReactNode => {
-   // Render if array
    if (Array.isArray(fieldValue))
       if (
          fieldValue.every(
@@ -50,10 +49,8 @@ const renderFieldValue = (fieldValue: FieldValue): ReactNode => {
          return fieldValue.map((item, index) => (
             <ul key={index}>{renderSingleField(item)}</ul>
          ));
-   // Render if object
    else if (fieldValue && typeof fieldValue === 'object')
-      return renderFields(fieldValue);
-   // Else
+      return <ul>{renderFields(fieldValue)}</ul>;
    else
       return (
          <>
@@ -70,46 +67,73 @@ const renderFieldValue = (fieldValue: FieldValue): ReactNode => {
  * @returns ReactNode
  */
 const renderSingleField = (field: FieldValue): ReactNode => {
-   // Render if array
    if (Array.isArray(field) && field.every((item) => typeof item === 'number'))
       return field.join(', ');
-   // Render if object
    else if (field && typeof field === 'object') return renderFields(field);
-   // Else
    else return <>{field === null || field === undefined ? 'null' : field}</>;
 };
 
 const App: FC = () => {
    const [jsonData, setJsonData] = useState<FieldObject[]>([]);
+   const [textareaValue, setTextareaValue] = useState<string>('');
+   const inputRef = useRef<HTMLInputElement>(null);
 
-   // Handler for file input upload
+   // Handle and convert file input uploaded
    const handleFileUpload = (e: ChangeEvent<HTMLInputElement>): void => {
-      const file = e.target.files?.[0];
+      const file: File | undefined = e.target.files?.[0];
       if (!file) return;
 
-      const reader = new FileReader();
-      reader.onload = () => {
-         try {
-            const json = JSON.parse(reader.result as string);
-
-            if (!Array.isArray(json)) setJsonData([json]);
-            else setJsonData(json);
-         } catch (error) {
-            alert('Errore nella lettura del file JSON.');
-         }
-      };
+      const reader: FileReader = new FileReader();
+      reader.onload = () => setJsonDataHandler(reader.result);
       reader.readAsText(file);
    };
 
+   // Handler for textarea insertion
+   const onTextareaValueChange = (e: ChangeEvent<HTMLTextAreaElement>): void =>
+      setTextareaValue(e.target.value);
+
+   // Visualize JSON on button click
+   const visualizeTextareaValue = (): void => {
+      setJsonDataHandler(textareaValue);
+      if (inputRef.current) inputRef.current.value = '';
+   };
+
+   // Manage json received and set json state
+   const setJsonDataHandler = (value: string | ArrayBuffer | null): void => {
+      try {
+         const json = JSON.parse(value as string);
+
+         if (!Array.isArray(json)) setJsonData([json]);
+         else setJsonData(json);
+      } catch (error) {
+         alert('Errore nella lettura del file JSON. Usa il formato corretto.');
+      }
+   };
+
    return (
-      <main>
-         <input type="file" accept=".json" onChange={handleFileUpload} />
-         <div className="container">
+      <div className="container">
+         <div className="header">
+            <input
+               type="file"
+               ref={inputRef}
+               accept=".json"
+               onChange={handleFileUpload}
+            />
+            <span>oppure</span>
+            <textarea
+               placeholder="Write in JSON"
+               value={textareaValue}
+               onChange={onTextareaValueChange}
+            ></textarea>
+            <button onClick={visualizeTextareaValue}>Visualize</button>
+         </div>
+
+         <main>
             {jsonData.map((item, key) => (
                <ul key={key}>{renderFields(item)}</ul>
             ))}
-         </div>
-      </main>
+         </main>
+      </div>
    );
 };
 
